a3698d075372d882c4241368c1d812954a358d71
tonnochiroad <jameswamorgan@gmail.com>
2017-03-30 14:38:59 +0100

    u1

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1490881113,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/frame-sheet0.png",
+		"images/cam_still-sheet0.png",
+		"images/cam_shift-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/capture-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
